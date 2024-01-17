026bd84f6502a64b67f2b0334272ca29567dcb89
Polvere243 <danielercoli@gmail.com>
2024-01-17 20:18:17 +0100

    copied a solution with a custom property, because classList didn't work properly

--- js/script.js
+++ js/script.js
@@ -3,11 +3,7 @@ const button = document.getElementById("play");
 const levelChoice = document.getElementById("level");
 const grid = document.getElementById("grid");
 const form = document.querySelector("form");
-const input = document.getElementById("score");
-
-
-
-
+const scoreCounter = document.querySelector("strong");
 
 // funzioni
 // dichiaro una funzione per generare le celle
@@ -16,9 +12,10 @@ const startGame = event => {
     event.preventDefault();
     // svuoto la griglia
     grid.innerText = "";
+    
     // funzioni nel gioco
     // funzione che genera la cella
-    const createCell = (cellNumber) => {
+    const createCell = cellNumber => {
         // creo la cella la stilizzo colla classe cell
         const cell = document.createElement("div");
         cell.classList.add("cell");
@@ -32,8 +29,6 @@ const startGame = event => {
     
     // recupero i valori dei livelli
     const level = levelChoice.value;
-    // determino la grandezza delle celle in base al valore
-    grid.classList.add(level);
     // determino la quantità di celle in base al value
     let rows = 10;
     let cols = 10;
@@ -46,25 +41,28 @@ const startGame = event => {
             rows = 7;
             cols = 7;
             break;
-            default:
     }
-    const totalCells = rows * cols;
-
-    let score = 0;
-    // genero le celle
-    for (let i = 0; i < totalCells; i++){
+        
+        // determino la grandezza delle celle in base al valore
+        const root = document.querySelector(":root");
+        root.style.setProperty("--cols-per-row", cols);
+        const totalCells = rows * cols;
+        let score = 0;
+        // genero le celle
+        for (let i = 0; i < totalCells; i++){
         const cell = createCell(i);
         
         // appendo le celle alla griglia
-        grid.appendChild(cell);
+        
         // aggancio al click sulle celle il toggle della classe clicked per colorare le suddette
         cell.addEventListener("click", () => {
             // impedisco di ricliccare una cella
             if (cell.classList.contains("clicked")) return;
             cell.classList.add("clicked");
             console.log(i);
-            score++
+            scoreCounter.innerText = ++score;
         });
+        grid.appendChild(cell);
     }
 
     // cell.addEventListener ("click", onCellCLicked)
